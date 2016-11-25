bc95fb13c7a1c725eaea1209e03ec7f710d4c39b
Vladimir Tazin <vladimir@wildapricot.com>
2016-11-25 14:08:28 +0300

    Add loader implementation

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
-module.exports = function(source, map)
-{
-  this.cacheable();
+var path = require('path');
+var lessLoader = require('less-loader');
+var loaderUtils = require('loader-utils');
 
-  this.callback(null, source, map);
+var themeConfigPattern = /^\.\.\/\.\.\/theme.config$/;
+var themePattern = /^\.\/theme\.less/;
+var folderPattern = /^\.\.\/(site|definitions|themes)(?=\/)/;
+var semanticUiLessRoot = path.dirname(
+  require.resolve('semantic-ui-less/package.json')
+);
+
+module.exports = function (source, map)
+{
+  if (this.cacheable)
+  {
+    this.cacheable();
+  }
+
+  var config = loaderUtils.getLoaderConfig(this, 'semanticUILessModuleLoader');
+
+  config.defaultFolder = config.defaultFolder || semanticUiLessRoot;
+  
+  config = Object.assign({
+    definitionsFolder: path.join(config.defaultFolder, 'definitions'),
+    siteFolder: path.join(config.defaultFolder, '_site'),
+    themeConfigPath: path.join(config.defaultFolder, 'theme.config.example'),
+    themePath: path.join(config.defaultFolder, 'theme.less'),
+    themesFolder: path.join(config.defaultFolder, 'themes')
+  }, config);
+
+  var originalResolve = this.resolve;
+
+  this.resolve = function(context, moduleRequest, callback)
+  {
+    moduleRequest = moduleRequest
+      .replace(themeConfigPattern, function() { return config.themeConfigPath; })
+      .replace(themePattern, function() { return config.themePath; })
+      .replace(folderPattern, function($0, dir) { return config[dir + 'Folder']; });
+
+    return originalResolve.call(this, context, moduleRequest, callback);
+  };
+
+  return lessLoader.call(this, source);
 };
